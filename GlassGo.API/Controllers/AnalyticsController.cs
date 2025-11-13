88009c5ed64f71df135a13bf28eb249ee957698f
Guillermo Howard <u202222275@upc.edu.pe>
2025-11-13 16:08:30 -0500

    feat: add controllers for analytics.

---
+++ GlassGo.API/Controllers/AnalyticsController.cs
@@ -0,0 +1,24 @@
+﻿using GlassGo.API.Analytics.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlassGo.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AnalyticsController : ControllerBase
+    {
+        private readonly ReportService _reportService;
+
+        public AnalyticsController(ReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+        [HttpGet("reports")]
+        public async Task<IActionResult> GetReports()
+        {
+            var reports = await _reportService.GetReportsAsync();
+            return Ok(reports);
+        }
+    }
+}
