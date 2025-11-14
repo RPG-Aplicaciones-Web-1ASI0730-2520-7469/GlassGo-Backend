2871c26b1ceed592370dd13c3547169cdd5d686e
FulLHous <u202211881@upc.edu.pe>
2025-11-13 22:52:45 -0500

    feat: add order domain interfaces for repository and services.

---
+++ GlassGo.API/ServicePlanning/Domain/Services/IOrderQueryService.cs
@@ -0,0 +1,11 @@
+﻿using GlassGo.API.ServicePlanning.Domain.Model.Aggregates;
+using GlassGo.API.ServicePlanning.Domain.Model.Queries;
+
+namespace GlassGo.API.ServicePlanning.Domain.Services;
+
+public interface IOrderQueryService
+{
+    Task<IEnumerable<Order>> Handle(GetAllOrdersQuery query);
+    Task<Order?> Handle(GetOrderByIdQuery query);
+}
+
